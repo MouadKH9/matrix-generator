f8c6e8c7016d92feab7be7ab9975d49b8b02c5bd
MouadKH9 <mouad.khchich@gmail.com>
2020-03-19 22:08:43 +0100

    Better sizing

--- functions.php
+++ functions.php
@@ -7,7 +7,7 @@
         $BRACKET_WIDTH = 405;
         $BRACKET_HEIGHT = 230;
 
-        $BRACKET_START_X = 150;
+        $BRACKET_START_X = 155;
         $BRACKET_START_Y = 85;
 
         $numberOfLines = count($data);
@@ -18,32 +18,22 @@
         $white = imagecolorallocate($img, 0, 0, 0);
         $font = "C:\Windows\Fonts\arial.ttf"; 
 
-        /*
-
-            brackets width: 145 -> 550
-            brackets height: 70 -> 300
-
-        */
-
-
-        $test = imagettftext($img, 40, 0, 10, $HEIGHT/2, $white, $font, "$name = ");
-        // var_dump($test);
-        // die();
+        imagettftext($img, 40, 0, 10, $HEIGHT/2, $white, $font, "$name = ");
 
         for ($i=0; $i < count($data); $i++) { 
             for ($j=0; $j < count($data[$i]); $j++) { 
                 $box = imagettfbbox(
-                            $BRACKET_WIDTH/(max([$numberOfCols,$numberOfLines]) * 2.2),
+                            $BRACKET_WIDTH/(max([$numberOfCols,$numberOfLines]) * 2.4),
                             0,
                             $font,
                             $data[$i][$j]
                     );
                 imagettftext(
                     $img, 
-                    $BRACKET_WIDTH/(max([$numberOfCols,$numberOfLines]) * 2.2),
+                    $BRACKET_WIDTH/(max([$numberOfCols,$numberOfLines]) * 2.4),
                     0,
-                    $BRACKET_START_X + $j * 470 / $numberOfCols,
-                    $BRACKET_START_Y + ($numberOfLines <= 3 ? max([$box[2], $box[4]]) : 0) + $i * 310  / $numberOfLines,
+                    $BRACKET_START_X + $j * 475 / $numberOfCols - ($box[2] - $box[0])/2,
+                    $BRACKET_START_Y + ($numberOfLines <= 3 ? max([$box[2], $box[4]]) : 0) + $i * 320  / $numberOfLines,
                     $white,
                     $font,
                     $data[$i][$j]
